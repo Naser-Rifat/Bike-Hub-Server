3571d05aaa2bb0a9ccb357264705922864cfd309
Naser Rifat <39612363+Naser-Rifat@users.noreply.github.com>
2021-11-14 00:59:42 +0600

    add

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ async function run() {
         const database = await client.db("cycleshop");
         const cyclescollection = await database.collection("cycles");
         const ordersdatacollection = await database.collection("orders");
+        const userdatacollection = await database.collection("users");
+        const reviewerdatacollection = await database.collection("reviews");
+
 
         app.get("/cycles", async (req, res) => {
             const query = await cyclescollection.find({});
@@ -33,6 +36,8 @@ async function run() {
             res.json(result);
 
         })
+
+        //get cycle info
         app.get("/cycles/:id", async (req, res) => {
             const id = req.params.id
 
@@ -42,6 +47,8 @@ async function run() {
             res.json(result);
 
         })
+
+        // add cycles info
         app.post("/cycles", async (req, res) => {
             const cycle = req.body;
             console.log(cycle);
@@ -70,6 +77,8 @@ async function run() {
             res.json(result);
 
         })
+
+
         //post a order
         app.post("/orders", async (req, res) => {
             const order = req.body;
@@ -101,6 +110,76 @@ async function run() {
             }
             const result = await ordersdatacollection.updateOne(filter, doc, options)
             console.log("counted", result);
+            res.json(result);
+
+        })
+
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await userdatacollection.insertOne(user)
+            console.log(result);
+            res.json(result);
+
+        })
+        app.put("/users", async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const filter = { email: user?.email }
+            const options = { upsert: true };
+            const doc = { $set: user }
+            const result = await userdatacollection.updateOne(filter, doc, options)
+            console.log(result);
+            res.json(result);
+
+        })
+        app.get("/users/:email", async (req, res) => {
+            const email = req.params.email;
+            console.log(email);
+            const filter = { email: email }
+            let IsAdmin = false;
+            const user = await userdatacollection.findOne(filter)
+            if (user?.role === 'admin') {
+                IsAdmin = true;
+
+
+            }
+            console.log(user);
+            res.json({ admin: IsAdmin });
+
+        })
+
+        app.put("/users/admin", async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const filter = { email: user?.email }
+            const doc = { $set: { role: "admin" } }
+            //let IsAdmin = false;
+            const result = await userdatacollection.updateOne(filter, doc)
+            // if (role === 'admin') {
+            //     IsAdmin = true;
+
+            // }
+            console.log(result);
+            //res.json({ admin: IsAdmin });
+            res.json(result);
+
+
+        })
+        //add review
+        app.post("/reviews", async (req, res) => {
+            const review = req.body;
+            console.log(review);
+            const result = await reviewerdatacollection.insertOne(review)
+
+            console.log(result);
+            res.json(result);
+
+        })
+        app.get("/reviews", async (req, res) => {
+            const query = await reviewerdatacollection.find({});
+            const result = await query.toArray();
+            console.log(result);
             res.json(result);
 
         })
